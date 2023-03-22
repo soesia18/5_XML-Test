aaf08764951afbd02179f409c4475243e1186526
simon <simon.schoeggler@hotmail.com>
2023-03-22 08:40:51 +0100

    Initial commit

---
+++ src/main/java/at/kaindorf/xml/Customer.java
@@ -0,0 +1,11 @@
+package at.kaindorf.xml;
+/**
+* <h3>Created by IntelliJ IDEA.</h3><br>
+* <b>Project:</b> XML-Test<br>
+* <b>User:</b> Simon Schoeggler<br>
+* <b>Date:</b> 22. März 2023<br>
+* <b>Time:</b> 08:18<br>
+*/
+
+public class Customer {
+}
